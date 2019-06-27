de6b5dbab76adeb9a4250fdcfc653cddb755b72d
Logan Schultz <schultzlogana@gmail.com>
2019-06-27 12:42:41 -0400

    Update Keys.java

--- src/main/java/Data/Keys.java
+++ src/main/java/Data/Keys.java
@@ -1,12 +1,5 @@
 package Data;
 
 public class Keys {
-    public final static String REDIRECT_URL = "https://login.microsoftonline.com/common/oauth2/nativeclient";
-    public final static String SCOPES = "User.Read Sites.FullControl.All Sites.ReadWrite.All Sites.Read.All";
-    public static final String ADD_RECORD_URL = "https://graph.microsoft.com/v1.0/sites/diskenterprisesolutions.sharepoint.com,b1b85f90-52ad-4c60-a1ce-b740797482d5,a80ce79c-2ab3-42e0-b91e-923332c20ae4/lists/4bdbf119-7849-4c26-92d3-126b7bf2d912/items";
-    public static final String GET_ALL_ITEMS_URL = "https://graph.microsoft.com/v1.0/sites/diskenterprisesolutions.sharepoint.com,b1b85f90-52ad-4c60-a1ce-b740797482d5,a80ce79c-2ab3-42e0-b91e-923332c20ae4/lists/4bdbf119-7849-4c26-92d3-126b7bf2d912/items/";
-    public static final String DELETE_RECORD_URL = "https://graph.microsoft.com/v1.0/sites/diskenterprisesolutions.sharepoint.com,b1b85f90-52ad-4c60-a1ce-b740797482d5,a80ce79c-2ab3-42e0-b91e-923332c20ae4/lists/4bdbf119-7849-4c26-92d3-126b7bf2d912/items/";
-    public static final String GET_ITEM_URL = "https://graph.microsoft.com/v1.0/sites/diskenterprisesolutions.sharepoint.com,b1b85f90-52ad-4c60-a1ce-b740797482d5,a80ce79c-2ab3-42e0-b91e-923332c20ae4/lists/4bdbf119-7849-4c26-92d3-126b7bf2d912/items/";
-    public static final String TSHEETS_KEY = "S.3__b643f811cc4b56ce46b7ee0b9a24da958717ec8e";
-    public static final String CLIENT_ID = "927f21e2-44a5-48a1-8a63-0d3e6bd4b7f3";
+    
 }
